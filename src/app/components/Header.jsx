b183fe90ddbd95bd92ea7c88ae99275e0f76a13f
R4izzZzz <zunbuu84mn@gmail.com>
2025-03-11 08:55:50 +0800

    asd

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Header() {
   return (
-    <div className="w-4/5 lg:w-1/2 xl:1/2 h-20 flex justify-around items-center rounded-full fixed bottom-2 left-center bg-[#182338]">
+    <div className="w-screen lg:w-1/2 xl:1/2 h-20 flex justify-around items-center rounded-full fixed bottom-2 left-center bg-[#182338]">
       <img
         src="https://media-hosting.imagekit.io//e96ac2fefee24fb2/screenshot_1740614481519.png?Expires=1835222484&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=iL-A2ZZDKzV52h-gNxvhBDqcpKqyo~RmaHMDht021SZb-TdR8oeI1l74Feq~jly2-CB48u6QH70S-YxgRjmCVY6xGwG2sQrPc5lrWVOKTmLWA8z00-bLmx3EfpZVvg2PjXP81qS~QFH~kQHqfwbIKyZrFXOmgTQGRnAdtkKNMtdfiJQ3RxNS4gxq9BjKB90izZHO~dHKl1WiL7jcpx0TnQ5q9K0DXUoNRL6m2QUk2p3KajpU2OROfgpqTsbUVmftVbIzJCmSVrR3gwpFO7r1BAIBFJn3rZYHXUxv3GABv6WcPkjxomTYU2r~bjScd~uqvcAavK5LyY1QniCmUK2zkQ__"
         className="h-4/5 w-auto"
